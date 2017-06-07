1383d10711354c0b64542735075b7f9b5cce789e
Allan Jeremy <aj.dev254@gmail.com>
2017-06-07 10:52:54 +0300

    First upload - Basic css & js files include - Using bootstrap and fontawesome - Landing page first iteration done

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home | Rain E-Learning System </title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <!-- Navigation -->
+    <header>
+      <nav class="nav">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#"><i class="glyphicon glyphicon-tint"></i> RAIN</a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#rainNav">
+              <i class="glyphicon glyphicon-menu-hamburger"></i>
+            </button>
+          </div>
+
+          <div class="collapse navbar-collapse row" id="rainNav">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">ABOUT</a></li>
+              <li><a href="#">FEATURES</a></li>
+              <li><a href="#">PRICING</a></li>
+              <li><a href="#">CONTACT</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+    </header>
+    <main>
+      <!-- Landing section here -->
+      <div class="landing row">
+        <div class="container">
+          <h1 class="light-text text-center">The simplest E-Learning system that gets things done</h1>
+          <div class="container card landing-card col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <h2 class="text-center">SIMPLE . ELEGANT . RESPONSIVE</h2>
+            <br>
+            <p class="large-text light-text text-center">
+              Upgrade to a smart digital solution for your institution with Rain
+            </p>
+            <br>
+            <a class="col-xs-6 col-sm-4 col-sm-offset-1 btn btn-landing" href="pricing.php" id="btn-landing-get">
+              GET
+            </a>
+            <a class="col-xs-6 col-sm-4 col-sm-offset-1 btn btn-landing" href="about.php" id="btn-landing-learn">
+              LEARN MORE
+            </a>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div class="container ">
+        <h2 class="text-center">FEATURES</h2>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="card card-fixed">
+            <h4 class="text-center">CLASSROOMS</h4>
+            <p class="text-center"><i class="fa fa-group fa-5x" aria-hidden="true"></i></p><br>
+            <h4 class="text-center light-text">
+              Manage groups of students through classrooms.
+            </h4>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="card card-fixed">
+            <h4 class="text-center">ASSIGNMENTS</h4>
+            <p class="text-center"><i class="fa fa-book fa-5x" aria-hidden="true"></i></p><br>
+            <h4 class="text-center light-text">
+              Send, work on, receive and grade assignments.
+            </h4>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="card card-fixed">
+            <h4 class="text-center">TESTS</h4>
+            <p class="text-center"><i class="fa fa-graduation-cap fa-5x" aria-hidden="true"></i></p><br>
+            <h4 class="text-center light-text">
+              Create, take and automatically grade tests.
+            </h4>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="card card-fixed">
+            <h4 class="text-center">SCHEDULING</h4>
+            <p class="text-center"><i class="fa fa-calendar-o fa-5x" aria-hidden="true"></i></p><br>
+            <h4 class="text-center light-text">
+              Plan lessons and mark student attendance
+            </h4>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="card card-fixed">
+            <h4 class="text-center">STATISTICS</h4>
+            <p class="text-center"><i class="fa fa-pie-chart fa-5x" aria-hidden="true"></i></p><br>
+            <h4 class="text-center light-text">
+              View assignment and schedule statistics.
+            </h4>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="card card-fixed">
+            <h4 class="text-center">SUPERUSER PANEL</h4>
+            <p class="text-center"><i class="fa fa-user-plus fa-5x" aria-hidden="true"></i></p><br>
+            <h4 class="text-center light-text"><h4 class="text-center light-text">
+              Create and manage user accounts through the superuser panel.
+            </h4>
+          </div>
+        </div>
+
+        <!-- Pricing -->
+        <div class="col-xs-12 col-sm-6">
+          <div class="card card-fixed">
+            <h4 class="text-center">PRICING</h4>
+            <p class="text-center"><i class="fa fa-money fa-5x" aria-hidden="true"></i></p><br>
+            <h4 class="text-center light-text">
+              Get a custom quotation based on your institution's needs
+            </h4>
+            <br>
+            <p class="text-center"><a class="btn btn-default">GET QUOTATION</a></p>
+          </div>
+        </div>
+
+        <!-- Contact -->
+        <div class="col-xs-12 col-sm-6">
+          <div class="card card-fixed">
+            <h4 class="text-center">CONTACT</h4>
+            <p class="text-center"><i class="fa fa-phone fa-5x" aria-hidden="true"></i></p><br>
+            <h4 class="text-center light-text">
+              Have any Questions? Feedback? We'd like to hear from you
+            </h4>
+            <br>
+            <p class="text-center"><a class="btn btn-default">CONTACT US</a></p>
+
+          </div>
+        </div>
+      </div>
+      <br><br>
+    </main>
+
+    <!-- Footer -->
+    <!--
+      faqs,terms and conditions,privacy statement,cookie policy
+      about, features,pricing,contact
+      sitemap
+    -->
+    <footer class="container-fluid footer ">
+      <div class="col-sm-3 col-sm-offset-3">
+        <br>
+        <ul class="footer-list">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+
+      <div class="col-sm-3">
+        <br>
+        <ul class="footer-list">
+          <li><a href="#">FAQs</a></li>
+          <li><a href="#">Terms &amp Conditions</a></li>
+          <li><a href="#">Privacy Statement</a></li>
+          <li><a href="#">Cookie Policy</a></li>
+        </ul>
+      </div>
+
+      <div class="col-xs-12 divider"></div>
+      <div class="col-xs-12">
+        <br>
+        <p class="text-center">&copy <?php echo date("Y");?> <a href="./">Rain E-Learning</a> <small>Developed by <a href="#!" target="blank">Deflix Enterprises</a> in partnership with <a href="#!" target="blank">Mwaura Muchiri</a></small></p>
+      </div>
+    </footer>
+  </body>
+</html>
